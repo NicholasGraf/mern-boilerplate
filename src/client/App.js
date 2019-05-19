1353ce59a8008d9bdaebbb957b4f766aaa7b6502
Nicholas Graf <ngraf@1stquad.com>
2019-05-19 13:04:12 +0800

    Tar update

--- src/client/App.js
+++ src/client/App.js
@@ -5,7 +5,22 @@ class App extends React.Component {
  
   render() {
       return (
+        <div className="main">
          <h1>Hello MERN Boilerplate!</h1>
+         <p>This is a MERN template app with:</p>
+         <ul>
+           <li>MongoDB</li>
+           <li>Express</li>
+           <li>React</li>
+           <li>ES6-ES8 Transpiling</li>
+           <li>Webpack</li>
+           <li>Sass</li>
+           <li>Linting</li>
+           <li>ENV Files</li>
+           <li>Dockerization</li>
+           <li>Server-side Rendering/Isomophic JS</li>
+         </ul>
+         </div>
       );
   }
 }
